6ed0678c133fb2af3e262cfc013e4ac8edeed6c5
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-02 09:53:25 +0200

    Add selectable numbers for rows and columns

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-var boxNumber = parseInt(prompt("Con quante caselle vuoi giocare?"));
+var btnPlay = document.getElementById("play");
 var playground = document.getElementById("playground");
 
-for (var i = 1; i <= boxNumber; i++) {
-    playground.innerHTML += `<div class="square">${i}</div>`;
-}
+btnPlay.addEventListener("click", 
+    function(){
+        //raccolgo gli input dell'utente per righe e colonne
+        var rows = parseInt(document.getElementById("rows").value);
+        var columns = parseInt(document.getElementById("columns").value);
+
+        //valido gli input dell'utente per righe e colonne
+        if(rows < 2 || rows > 10 || columns < 2 || columns > 10) {
+            return alert("Attenzione! Devi inserire un numero compreso da 2 a 10 per righe e colonne!");
+        } else {
+            //calcolo il numero di caselle moltiplicando righe per colonne
+            var boxNumber = rows * columns;
+
+            //genero gli elementi nell'HTML con un ciclo for
+            for (var i = 1; i <= boxNumber; i++) {
+                playground.innerHTML += `<div class="square">${i}</div>`;
+            }
+
+            //aggiungo all'elemento con id "playground" la classe appropriata per il numero di colonne
+            playground.classList.add("col_" + columns);
+        }
+    }
+)
+
 
 playground.addEventListener("click",
     function(clicked) {
@@ -11,3 +32,4 @@ playground.addEventListener("click",
         alert(clicked.target.innerHTML)
     }
 )
+
